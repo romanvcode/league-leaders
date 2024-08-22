c99df06eed4c6cdd38dcb9ef911a4a958871cf50
leobit-rvorobel <rvorobel@leobit.com>
2024-08-22 12:11:35 +0300

    added table for each entity

--- api/LeagueLeaders.Infrastructure/LeagueLeadersDbContext.cs
+++ api/LeagueLeaders.Infrastructure/LeagueLeadersDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LeagueLeaders.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace LeagueLeaders.Infrastructure
 {
@@ -7,5 +8,17 @@ namespace LeagueLeaders.Infrastructure
         public LeagueLeadersDbContext(DbContextOptions<LeagueLeadersDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Competition> Competitions { get; set; }
+        public DbSet<Season> Seasons { get; set; }
+        public DbSet<Stage> Stages { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Referee> Referees { get; set; }
+        public DbSet<PlayerStat> PlayerStats { get; set; }
+        public DbSet<TeamStat> TeamStats { get; set; }
+        public DbSet<Standing> Standings { get; set; }
     }
 }
