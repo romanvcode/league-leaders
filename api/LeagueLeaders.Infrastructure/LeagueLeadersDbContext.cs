9aaade30791eec77db2648e081a0c07d3bd71a36
leobit-rvorobel <rvorobel@leobit.com>
2024-08-22 11:47:28 +0300

    added Db Context for project with references to EF Core

---
+++ api/LeagueLeaders.Infrastructure/LeagueLeadersDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LeagueLeaders.Infrastructure
+{
+    public class LeagueLeadersDbContext : DbContext
+    {
+        public LeagueLeadersDbContext(DbContextOptions<LeagueLeadersDbContext> options) : base(options)
+        {
+        }
+    }
+}
