ef998ac739afd921057a9f8df35e60a8b4ef79ac
leobit-rvorobel <rvorobel@leobit.com>
2024-08-27 17:33:50 +0300

    removed unnecessary options

--- api/src/LeagueLeaders.API/Program.cs
+++ api/src/LeagueLeaders.API/Program.cs
@@ -8,9 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllers().AddNewtonsoftJson(options =>
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-);
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<LeagueLeadersDbContext>(options =>
 {
