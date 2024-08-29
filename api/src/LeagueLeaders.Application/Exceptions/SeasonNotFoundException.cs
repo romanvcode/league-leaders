980b48de8ef5f5f65ba51660a1ac3aa6e833b659
leobit-rvorobel <rvorobel@leobit.com>
2024-08-29 17:55:11 +0300

    added custom exceptions for services

---
+++ api/src/LeagueLeaders.Application/Exceptions/SeasonNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace LeagueLeaders.Application.Exceptions
+{
+    public class SeasonNotFoundException : Exception
+    {
+        public SeasonNotFoundException() { }
+
+        public SeasonNotFoundException(string message)
+            : base(message) { }
+
+        public SeasonNotFoundException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
---
+++ api/src/LeagueLeaders.Application/Exceptions/TeamNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace LeagueLeaders.Application.Exceptions
+{
+    public class TeamNotFoundException : Exception
+    {
+        public TeamNotFoundException() { }
+
+        public TeamNotFoundException(string message)
+            : base(message) { }
+
+        public TeamNotFoundException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
