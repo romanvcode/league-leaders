261bf134ae0e3d16738732498515b0b923110580
leobit-rvorobel <rvorobel@leobit.com>
2024-09-02 13:43:59 +0300

    added custom validator

---
+++ api/src/LeagueLeaders.API/Validators/TeamIdValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace LeagueLeaders.API.Validators;
+
+public class TeamIdValidator : AbstractValidator<int>
+{
+    public TeamIdValidator()
+    {
+        RuleFor(x => x)
+            .GreaterThan(0)
+            .WithMessage("Team ID must be greater than 0.");
+    }
+}
