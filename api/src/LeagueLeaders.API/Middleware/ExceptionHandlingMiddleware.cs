a67f098c2a94f63e06bbaa9efcb96875fa070e9b
leobit-rvorobel <rvorobel@leobit.com>
2024-09-02 11:27:39 +0300

    added Exception Middlware to rebase common logic

---
+++ api/src/LeagueLeaders.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿
+using LeagueLeaders.Application;
+
+namespace LeagueLeaders.API.Middleware;
+
+public class ExceptionHandlingMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next.Invoke(context);
+        }
+        catch (TeamNotFoundException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsync(ex.Message);
+        }
+        catch (SeasonNotFoundException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsync(ex.Message);
+        }
+        catch (StageNotFoundException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsync(ex.Message);
+        }
+        catch (StandingsNotFoundException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsync(ex.Message);
+        }
+        catch (ArgumentNullException ex) when (ex.ParamName == "teamId")
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsync(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsync(ex.Message);
+        }
+    }
+}
