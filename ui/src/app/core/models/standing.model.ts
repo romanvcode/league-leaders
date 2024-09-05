db90396dc1364df2df3ad68274e122d9473f2146
leobit-rvorobel <rvorobel@leobit.com>
2024-09-05 18:17:07 +0300

    added standing model

---
+++ ui/src/app/core/models/standing.model.ts
@@ -0,0 +1,11 @@
+export interface Standing {
+  place: number;
+  name: string;
+  matchesPlayed: number;
+  wins: number;
+  draws: number;
+  losses: number;
+  goalsFor: number;
+  goalsAgainst: number;
+  points: number;
+}
