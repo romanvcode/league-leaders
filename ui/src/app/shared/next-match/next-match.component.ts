8570985a91a256e339aa50d1d08ba202e421f983
leobit-rvorobel <rvorobel@leobit.com>
2024-09-09 11:46:11 +0300

    updated logic to use signals and to show remaining days

--- ui/src/app/shared/next-match/next-match.component.ts
+++ ui/src/app/shared/next-match/next-match.component.ts
@@ -1,7 +1,8 @@
-import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Match } from '@core/models/match.model';
 import { ApiService } from '@core/services/api.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-next-match',
@@ -10,40 +11,49 @@ import { ApiService } from '@core/services/api.service';
   templateUrl: './next-match.component.html',
   styleUrl: './next-match.component.css',
 })
-export class NextMatchComponent implements OnInit {
+export class NextMatchComponent implements OnInit, OnDestroy {
   match = signal<Match | undefined>(undefined);
   error = signal('');
+  subscription = signal<Subscription | undefined>(undefined);
+  interval = signal<Subscription | undefined>(undefined);
   countdown = signal('');
 
-  private apiService = inject(ApiService);
-  private destroyRef = inject(DestroyRef);
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    const subscription = this.apiService.getMathces().subscribe({
-      next: (matches) => {
-        this.match.set(matches.shift());
-        this.startCountdown(new Date(this.match()!.date));
-      },
-      error: (error) => {
-        console.error(error);
-        this.error.set('An error occurred while fetching matches');
-      },
-    });
-
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    this.subscription.set(
+      this.apiService.getMathces().subscribe({
+        next: (matches) => {
+          this.match.set(matches.shift());
+          this.startCountdown(new Date(this.match()!.date));
+        },
+        error: (error) => {
+          console.error(error);
+          this.error.set('An error occurred while fetching matches');
+        },
+      })
+    );
   }
 
   startCountdown(startTime: Date): void {
-    const interval = setInterval(() => {
-      const now = new Date().getTime();
-      const distance = startTime.getTime() - now;
+    this.interval.set(
+      interval(1000).subscribe(() => {
+        const now = new Date().getTime();
+        const distance = startTime.getTime() - now;
 
-      const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      this.countdown.set(`${hours}h ${minutes}m ${seconds}s`);
-    }, 1000);
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        this.countdown.set(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription()?.unsubscribe();
+    this.interval()?.unsubscribe();
   }
 }
