b3834b5593cc124fee9e47157c9dad8fde3dd541
leobit-rvorobel <rvorobel@leobit.com>
2024-09-09 17:39:15 +0300

    added tests for feature component

--- ui/src/app/features/landing/landing.component.spec.ts
+++ ui/src/app/features/landing/landing.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ApiService } from '@core/services/api.service';
 import { LandingComponent } from './landing.component';
 
 describe('LandingComponent', () => {
@@ -9,6 +12,7 @@ describe('LandingComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LandingComponent],
+      providers: [HttpClient, HttpHandler, ApiService, provideAnimations()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LandingComponent);
@@ -18,5 +22,9 @@ describe('LandingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a title', () => {
+    expect(component.curretFeature).toBe('Leaderboard');
   });
 });
