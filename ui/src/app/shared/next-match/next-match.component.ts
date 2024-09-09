a926594b86546e2c4025e69b3461280788537ecb
leobit-rvorobel <rvorobel@leobit.com>
2024-09-09 09:18:39 +0300

    updated next match to show min info

--- ui/src/app/shared/next-match/next-match.component.ts
+++ ui/src/app/shared/next-match/next-match.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { Match } from '@core/models/match.model';
+import { ApiService } from '@core/services/api.service';
 
 @Component({
   selector: 'app-next-match',
@@ -8,4 +10,40 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './next-match.component.html',
   styleUrl: './next-match.component.css',
 })
-export class NextMatchComponent {}
+export class NextMatchComponent implements OnInit {
+  match = signal<Match | undefined>(undefined);
+  error = signal('');
+  countdown = signal('');
+
+  private apiService = inject(ApiService);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.apiService.getMathces().subscribe({
+      next: (matches) => {
+        this.match.set(matches.shift());
+        this.startCountdown(new Date(this.match()!.date));
+      },
+      error: (error) => {
+        console.error(error);
+        this.error.set('An error occurred while fetching matches');
+      },
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
+  startCountdown(startTime: Date): void {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = startTime.getTime() - now;
+
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      this.countdown.set(`${hours}h ${minutes}m ${seconds}s`);
+    }, 1000);
+  }
+}
