58658ccd3529fee705adb7a085e717dd64f5dfb6
leobit-rvorobel <rvorobel@leobit.com>
2024-09-11 16:43:14 +0300

    added model for player

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TeamComponent } from '@features/team/team.component';
 import { LandingComponent } from './features/landing/landing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LandingComponent],
+  imports: [LandingComponent, TeamComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
