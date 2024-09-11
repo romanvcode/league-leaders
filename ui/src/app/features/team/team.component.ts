6da99344fbcbe4bd3e90e821eebb0710618febcb
leobit-rvorobel <rvorobel@leobit.com>
2024-09-11 14:24:29 +0300

    generated team component

---
+++ ui/src/app/features/team/team.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team',
+  standalone: true,
+  imports: [],
+  templateUrl: './team.component.html',
+  styleUrl: './team.component.css',
+})
+export class TeamComponent {}
