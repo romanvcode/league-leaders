1dd8dc92f97c9c2a4f32a7841b06ea770895d013
leobit-rvorobel <rvorobel@leobit.com>
2024-09-13 17:54:33 +0300

    implemented search input

--- ui/src/app/shared/search-input/search-input.component.spec.ts
+++ ui/src/app/shared/search-input/search-input.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { ApiService } from '@core/services/api.service';
 import { SearchInputComponent } from './search-input.component';
 
 describe('SearchInputComponent', () => {
@@ -10,7 +13,37 @@ describe('SearchInputComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SearchInputComponent],
-      providers: [provideAnimations()],
+      providers: [
+        ApiService,
+        provideAnimations(),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SearchInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+describe('SearchInputComponent', () => {
+  let component: SearchInputComponent;
+  let fixture: ComponentFixture<SearchInputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SearchInputComponent],
+      providers: [
+        ApiService,
+        provideAnimations(),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchInputComponent);
--- ui/src/app/shared/search-input/search-input.component.ts
+++ ui/src/app/shared/search-input/search-input.component.ts
@@ -1,9 +1,13 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForOf, NgIf } from '@angular/common';
+import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { ApiService } from '@core/services/api.service';
+import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-search-input',
@@ -14,16 +18,50 @@ import { MatInputModule } from '@angular/material/input';
     FormsModule,
     MatButtonModule,
     MatIconModule,
+    MatAutocompleteModule,
+    NgIf,
+    NgForOf,
   ],
   templateUrl: './search-input.component.html',
   styleUrl: './search-input.component.css',
 })
-export class SearchInputComponent {
+export class SearchInputComponent implements OnDestroy {
+  private destroy$ = new Subject<void>();
+
   @Output() search = new EventEmitter<string>();
 
   value: string = '';
+  searchResults: string[] = [];
+
+  constructor(private apiService: ApiService) {}
 
   onSearch() {
-    this.search.emit(this.value);
+    if (this.value) {
+      this.apiService
+        .getTeamsBySearchTerm(this.value)
+        .pipe(takeUntil(this.destroy$))
+        .pipe(debounceTime(300), distinctUntilChanged())
+        .subscribe((teams) => {
+          this.searchResults = teams.map((team) => team.name);
+        });
+    } else {
+      this.clearSearch();
+    }
+  }
+
+  onSelect(result: string) {
+    this.search.emit(result);
+    this.value = result;
+  }
+
+  clearSearch() {
+    this.value = '';
+    this.searchResults = [];
+    this.search.emit('');
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
