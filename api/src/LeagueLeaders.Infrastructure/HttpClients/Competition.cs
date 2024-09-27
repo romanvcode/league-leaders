4b8a2e3ffa23a5766decb2c803d69f458936ab02
leobit-rvorobel <rvorobel@leobit.com>
2024-09-27 11:00:38 +0300

    provided model for Competition

---
+++ api/src/LeagueLeaders.Infrastructure/HttpClients/Competition.cs
@@ -0,0 +1,12 @@
+﻿namespace LeagueLeaders.Infrastructure.HttpClients;
+public class Competition
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public Category Category { get; set; }
+}
+
+public class Category
+{
+    public string CountryCode { get; set; }
+}
