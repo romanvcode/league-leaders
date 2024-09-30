18d7266d0f5312ac8e734ed6c4ea81dcd1230497
leobit-rvorobel <rvorobel@leobit.com>
2024-09-30 12:14:23 +0300

    added custom exception for deserialization error

---
+++ api/src/LeagueLeaders.Infrastructure/Clients/SportradarApi/Exceptions/DeserializationException.cs
@@ -0,0 +1,16 @@
+﻿namespace LeagueLeaders.Infrastructure.Clients.SportradarApi.Exceptions;
+
+public class DeserializationException : Exception
+{
+    public DeserializationException()
+    {
+    }
+
+    public DeserializationException(string message) : base(message)
+    {
+    }
+
+    public DeserializationException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
