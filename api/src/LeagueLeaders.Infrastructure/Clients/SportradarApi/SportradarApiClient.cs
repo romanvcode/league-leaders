4ae9dd129fb74464a1b520a3f10e22c300208888
leobit-rvorobel <rvorobel@leobit.com>
2024-09-30 19:42:47 +0300

    added new methods to client - tested with controller

--- api/src/LeagueLeaders.Infrastructure/Clients/SportradarApi/SportradarApiClient.cs
+++ api/src/LeagueLeaders.Infrastructure/Clients/SportradarApi/SportradarApiClient.cs
@@ -27,9 +27,9 @@ public class SportradarApiClient
 
     public async Task<Models.Competition> GetCompetitionAsync()
     {
-        var competitionResponse = await GetResponse<CompetitionResponse>($"competitions/{_championsLeague}/info?api_key={_apiKey}") 
+        var competitionResponse = await GetResponse<CompetitionResponse>($"competitions/{_championsLeague}/info?api_key={_apiKey}")
             ?? throw new DeserializationException($"Failed to deserialize competition: {_championsLeague}.");
-        
+
         var competition = new Models.Competition
         {
             Id = competitionResponse.Competition.Id,
@@ -42,9 +42,9 @@ public class SportradarApiClient
 
     public async Task<List<Models.Season>> GetSeasonsAsync()
     {
-        var seasonResponse = await GetResponse<SeasonsResponse>($"competitions/{_championsLeague}/seasons?api_key={_apiKey}") 
+        var seasonResponse = await GetResponse<SeasonsResponse>($"competitions/{_championsLeague}/seasons?api_key={_apiKey}")
             ?? throw new DeserializationException($"Failed to deserialize seasons for the {_championsLeague} competition.");
-        
+
         var seasons = seasonResponse.Seasons;
 
         return seasons;
@@ -52,9 +52,9 @@ public class SportradarApiClient
 
     public async Task<List<Models.Stage>> GetStagesAsync()
     {
-        var stageResponse = await GetResponse<StagesResponse>($"seasons/{_currentSeason}/stages_groups_cup_rounds?api_key={_apiKey}") 
+        var stageResponse = await GetResponse<StagesResponse>($"seasons/{_currentSeason}/stages_groups_cup_rounds?api_key={_apiKey}")
             ?? throw new DeserializationException($"Failed to deserialize stages for the {_currentSeason}.");
-        
+
         var stages = stageResponse.Stages;
 
         foreach (var stage in stages)
@@ -67,14 +67,14 @@ public class SportradarApiClient
 
     public async Task<List<Models.Competitor>> GetCompetitorsAsync()
     {
-        var competitorsResponse = await GetResponse<CompetitorsResponse>($"seasons/{_currentSeason}/competitors?api_key={_apiKey}") 
+        var competitorsResponse = await GetResponse<CompetitorsResponse>($"seasons/{_currentSeason}/competitors?api_key={_apiKey}")
             ?? throw new DeserializationException($"Failed to deserialize competitors for the {_currentSeason}.");
-        
+
         var competitors = competitorsResponse.SeasonCompetitors;
 
         foreach (var competitor in competitors)
         {
-            var profileResponse = await GetResponse<CompetitorProfileResponse>($"seasons/{_currentSeason}/competitors?api_key={_apiKey}");
+            var profileResponse = await GetResponse<CompetitorProfileResponse>($"competitors/{competitor.Id}/profile?api_key={_apiKey}");
 
             if (profileResponse == null)
             {
@@ -96,10 +96,14 @@ public class SportradarApiClient
 
         var sportEvents = sportEventsResponse.Summaries;
 
-        List<Models.SportEvent> events = [];
-
+        var events = new List<Models.SportEvent>();
         foreach (var sportEvent in sportEvents)
         {
+            if (sportEvent.SportEvent.SportEventConditions.Referees == null)
+            {
+                continue;
+            }
+
             events.Add(new Models.SportEvent
             {
                 Id = sportEvent.SportEvent.Id,
@@ -108,10 +112,84 @@ public class SportradarApiClient
                 AwayCompetitorId = sportEvent.SportEvent.Competitors.Single(c => c.Qualifier == "away").Id,
                 Date = sportEvent.SportEvent.StartTime,
                 VenueId = sportEvent.SportEvent.Venue.Id,
+                RefereeId = sportEvent.SportEvent.SportEventConditions.Referees.Single(r => r.Type == "main_referee").Id,
+                HomeCompetitorScore = sportEvent.SportEventStatus.HomeScore,
+                AwayCompetitorScore = sportEvent.SportEventStatus.AwayScore
             });
         }
 
         return events;
+    }
+
+    public async Task<List<Models.Player>> GetPlayersAsync()
+    {
+        var playersResponse = await GetResponse<PlayersResponse>($"seasons/{_currentSeason}/competitor_players?api_key={_apiKey}")
+            ?? throw new DeserializationException($"Failed to deserialize players for the {_currentSeason}.");
+
+        var competitorPlayers = playersResponse.SeasonCompetitorPlayers;
+
+        var players = new List<Models.Player>();
+        foreach (var competitorPlayer in competitorPlayers)
+        {
+            if (competitorPlayer.Players == null)
+            {
+                continue;
+            }
+
+            players.AddRange(competitorPlayer.Players.Select(p => new Models.Player
+            {
+                Id = p.Id,
+                CompetitorId = competitorPlayer.Id,
+                Name = p.Name,
+                Type = p.Type,
+                JerseyNumber = p.JerseyNumber,
+                Height = p.Height,
+                Nationality = p.Nationality,
+                DateOfBirth = p.DateOfBirth
+            }));
+        }
+
+        return players;
+    }
+
+    public async Task<List<Models.Venue>> GetVenuesAsync()
+    {
+        var venuesResponse = await GetResponse<VenuesResponse>($"seasons/{_currentSeason}/summaries?api_key={_apiKey}")
+            ?? throw new DeserializationException($"Failed to deserialize venues for the {_currentSeason}.");
+
+        var venues = new List<Models.Venue>();
+        foreach (var venue in venuesResponse.Summaries.Select(s => s.SportEvent.Venue))
+        {
+            venues.Add(new Models.Venue
+            {
+                Id = venue.Id,
+                Name = venue.Name,
+                CityName = venue.CityName,
+                CountryName = venue.CountryName,
+                Capacity = venue.Capacity
+            });
+        }
+
+        return venues;
+    }
+
+    public async Task<List<Referee>> GetRefereesAsync()
+    {
+        var refereesResponse = await GetResponse<RefereesResponse>($"seasons/{_currentSeason}/summaries?api_key={_apiKey}")
+            ?? throw new DeserializationException($"Failed to deserialize referees for the {_currentSeason}.");
+
+        var referees = new List<Referee>();
+        foreach (var se in refereesResponse.Summaries.Select(s => s.SportEvent))
+        {
+            if (se.SportEventConditions == null || se.SportEventConditions.Referees == null)
+            {
+                continue;
+            }
+
+            referees.Add(se.SportEventConditions.Referees.Single(r => r.Type == "main_referee"));
+        }
+
+        return referees;
     }
 
     private async Task<T?> GetResponse<T>(string url)
