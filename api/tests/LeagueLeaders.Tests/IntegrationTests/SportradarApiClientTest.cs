f6ffd6e2052cb59b01d7b0490efe9f27abdb0677
leobit-rvorobel <rvorobel@leobit.com>
2024-10-02 17:36:24 +0300

    added test class for typed http client

---
+++ api/tests/LeagueLeaders.Tests/IntegrationTests/SportradarApiClientTest.cs
@@ -0,0 +1,35 @@
+﻿using LeagueLeaders.Infrastructure.Clients.SportradarApi;
+using Microsoft.Extensions.Options;
+using FluentAssertions;
+using Moq;
+using Moq.Protected;
+using System.Net;
+using FluentAssertions.Execution;
+
+namespace LeagueLeaders.Tests.IntegrationTests;
+
+public class SportradarApiClientTest
+{
+    private readonly IOptions<SportradarSettings> _options;
+    private readonly ISportradarApiClient _sportradarApiClient;
+    private readonly HttpClient _httpClient;
+    private readonly Mock<HttpMessageHandler> _httpMessageHandlerMock;
+
+    public SportradarApiClientTest()
+    {
+        _options = Options.Create(new SportradarSettings
+        {
+            ApiKey = "test-api-key",
+            ChampionsLeague = "test-league",
+            CurrentSeason = "test-season"
+        });
+
+        _httpMessageHandlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+        _httpClient = new HttpClient(_httpMessageHandlerMock.Object)
+        {
+            BaseAddress = new Uri("https://api.sportradar.com")
+        };
+
+        _sportradarApiClient = new SportradarApiClient(_httpClient, _options);
+    }
+}
