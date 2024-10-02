99b11214059ee9cbe6193fd9c1caedbfd8daee8a
leobit-rvorobel <rvorobel@leobit.com>
2024-10-02 09:55:19 +0300

    provided new exceptions

---
+++ api/src/LeagueLeaders.Infrastructure/Clients/SportradarApi/Exceptions/SportradarBadResponseException.cs
@@ -0,0 +1,16 @@
+﻿namespace LeagueLeaders.Infrastructure.Clients.SportradarApi.Exceptions;
+
+public class SportradarBadResponseException : Exception
+{
+    public SportradarBadResponseException()
+    {
+    }
+
+    public SportradarBadResponseException(string message) : base(message)
+    {
+    }
+
+    public SportradarBadResponseException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ api/src/LeagueLeaders.Infrastructure/Clients/SportradarApi/Exceptions/SportradarSettingsNullException.cs
@@ -0,0 +1,16 @@
+﻿namespace LeagueLeaders.Infrastructure.Clients.SportradarApi.Exceptions;
+
+public class SportradarSettingsNullException : Exception
+{
+    public SportradarSettingsNullException()
+    {
+    }
+
+    public SportradarSettingsNullException(string message) : base(message)
+    {
+    }
+
+    public SportradarSettingsNullException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
