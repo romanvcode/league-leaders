8bb64f2bbbd27b69786ffe0744000a9ac4139b62
leobit-rvorobel <rvorobel@leobit.com>
2024-10-04 17:32:25 +0300

    created service for synchronization

---
+++ api/src/LeagueLeaders.Application/ApiDataSyncService.cs
@@ -0,0 +1,39 @@
+﻿using LeagueLeaders.Domain;
+using LeagueLeaders.Infrastructure.Clients.SportradarApi;
+using LeagueLeaders.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeagueLeaders.Application;
+
+public class ApiDataSyncService
+{
+    private readonly LeagueLeadersDbContext _context;
+    private readonly ISportradarApiClient _sportradarApiClient;
+    private readonly string _competitionPrefix = "sr:competition:";
+
+    public ApiDataSyncService(LeagueLeadersDbContext context, ISportradarApiClient sportradarApiClient)
+    {
+        _context = context;
+        _sportradarApiClient = sportradarApiClient;
+    }
+
+    public async Task SyncDataAsync()
+    {
+        var srCompetition = await _sportradarApiClient.GetCompetitionAsync();
+        var dbCompetition = await _context.Competitions.FirstOrDefaultAsync
+            (competiton => $"{_competitionPrefix}{competiton.SportradarId}" == srCompetition.Id);
+
+        if (dbCompetition == null)
+        {
+            await _context.Competitions.AddAsync(new Competition
+            {
+                Name = srCompetition.Name,
+                SportradarId = int.Parse(srCompetition.Id.ToLower().Replace(_competitionPrefix, ""))
+            });
+        }
+
+        // ...
+
+        await _context.SaveChangesAsync();
+    }
+}
