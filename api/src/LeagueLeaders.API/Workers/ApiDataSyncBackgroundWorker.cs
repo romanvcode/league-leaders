37a7bc47113168afbf2cf9c565ab79e0174efbe5
leobit-rvorobel <rvorobel@leobit.com>
2024-10-07 13:06:07 +0300

    provided background worker for sync

---
+++ api/src/LeagueLeaders.API/Workers/ApiDataSyncBackgroundWorker.cs
@@ -0,0 +1,52 @@
+﻿using LeagueLeaders.Application.ApiDataSync;
+using Microsoft.Extensions.Options;
+
+namespace LeagueLeaders.API.Workers;
+
+public class ApiDataSyncBackgroundWorker : BackgroundService
+{
+    private readonly ILogger<ApiDataSyncBackgroundWorker> _logger;
+    private readonly IApiDataSyncService _syncService;
+    private readonly SyncSettings _syncSettings;
+
+    public ApiDataSyncBackgroundWorker(
+        ILogger<ApiDataSyncBackgroundWorker> logger,
+        IApiDataSyncService service, 
+        IOptions<SyncSettings> options)
+    {
+        _logger = logger;
+        _syncService = service;
+        _syncSettings = options.Value;
+    }
+
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("ApiDataSyncBackgroundWorker started.");
+
+        await PerformSyncDataAsync();
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Waiting for next sync...");
+
+            await Task.Delay(TimeSpan.FromHours(_syncSettings.FetchFrequencyHours), stoppingToken);
+
+            await PerformSyncDataAsync();
+        }
+    }
+
+    private async Task PerformSyncDataAsync()
+    {
+        _logger.LogInformation("Fetching data from API...");
+
+        try
+        {
+            await _syncService.SyncDataAsync();
+            _logger.LogInformation("API sync completed successfully.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while syncing API data.");
+        }
+    }
+}
