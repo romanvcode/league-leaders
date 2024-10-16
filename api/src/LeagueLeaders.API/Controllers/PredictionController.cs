e0f10bdca4214f8ef14d0ad48066a56859cdd9bb
leobit-rvorobel <rvorobel@leobit.com>
2024-10-16 17:24:46 +0300

    provided controller for Predictions

---
+++ api/src/LeagueLeaders.API/Controllers/PredictionController.cs
@@ -0,0 +1,32 @@
+﻿using LeagueLeaders.Application.Predictions;
+using LeagueLeaders.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeagueLeaders.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class PredictionController : ControllerBase
+{
+    private readonly IPredictionService _predictionService;
+
+    public PredictionController(IPredictionService predictionService)
+    {
+        _predictionService = predictionService;
+    }
+
+    [HttpPost]
+    public async Task<Prediction> CreatePredictionAsync(int matchId, int homeTeamScore, int awayTeamScore)
+    {
+        var prediction = await _predictionService.CreatePredictionAsync(matchId, homeTeamScore, awayTeamScore);
+
+        return prediction;
+    }
+
+    [HttpGet]
+    public async Task<List<Prediction>> GetPredictionsAsync()
+    {
+        var predicitons = await _predictionService.GetPredictionsAsync();
+
+        return predicitons;
+    }
+}
