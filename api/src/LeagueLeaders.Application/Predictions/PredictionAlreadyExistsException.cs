70a725409755f37c1660b09bc2e98c3ead0c49fb
leobit-rvorobel <rvorobel@leobit.com>
2024-10-16 17:23:28 +0300

    provided custom exception for Predictions

---
+++ api/src/LeagueLeaders.Application/Predictions/PredictionAlreadyExistsException.cs
@@ -0,0 +1,16 @@
+﻿namespace LeagueLeaders.Application.Predictions;
+
+public class PredictionAlreadyExistsException : Exception
+{
+    public PredictionAlreadyExistsException()
+    {
+    }
+
+    public PredictionAlreadyExistsException(string message) : base(message)
+    {
+    }
+
+    public PredictionAlreadyExistsException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
