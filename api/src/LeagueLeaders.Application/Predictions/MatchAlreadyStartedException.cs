da9dfd3da0c6cbf3e5192bcb2812409b010ab82a
leobit-rvorobel <rvorobel@leobit.com>
2024-10-16 19:59:09 +0300

    provided ex for match that started

---
+++ api/src/LeagueLeaders.Application/Predictions/MatchAlreadyStartedException.cs
@@ -0,0 +1,15 @@
+﻿namespace LeagueLeaders.Application.Predictions;
+
+public class MatchAlreadyStartedException : Exception
+{
+    public MatchAlreadyStartedException()
+    {
+    }
+    public MatchAlreadyStartedException(string message) : base(message)
+    {
+    }
+
+    public MatchAlreadyStartedException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
