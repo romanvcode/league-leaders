fa106181ed4ce2bca0c3a7cdd47c6a379a4180bb
leobit-rvorobel <rvorobel@leobit.com>
2024-10-16 17:10:52 +0300

    provided domain model for Predictions

---
+++ api/src/LeagueLeaders.Domain/Prediction.cs
@@ -0,0 +1,11 @@
+﻿namespace LeagueLeaders.Domain;
+
+public class Prediction
+{
+    public int Id { get; set; }
+    public int HomeTeamScore { get; set; }
+    public int AwayTeamScore { get; set; }
+    public int MatchId { get; set; }
+
+    public Match Match { get; set; } = null!;
+}
