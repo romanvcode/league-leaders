83bb5f11b3b106587ba45d6266bd02a6d30ae029
leobit-rvorobel <rvorobel@leobit.com>
2024-10-16 19:59:36 +0300

    added validators for match id and score

---
+++ api/src/LeagueLeaders.API/Validators/MatchIdValidor.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace LeagueLeaders.API.Validators;
+
+public class MatchIdValidor : AbstractValidator<int>
+{
+    public MatchIdValidor()
+    {
+        RuleFor(x => x)
+            .GreaterThan(0)
+            .WithMessage("Match ID must be greater than 0.");
+    }
+}
