40d1c97c3c51bd313a60a59aadfb0041f8d998f9
leobit-rvorobel <rvorobel@leobit.com>
2024-10-18 11:17:34 +0300

    updated form with new styles and validation

--- ui/src/app/shared/prediction-form/prediction-form.component.ts
+++ ui/src/app/shared/prediction-form/prediction-form.component.ts
@@ -1,3 +1,4 @@
+import { NgIf } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,6 +10,8 @@ import {
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Match } from '@core/models/match.model';
+import { ApiService } from '@core/services/api.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-prediction-form',
@@ -20,30 +23,51 @@ import { Match } from '@core/models/match.model';
     MatLabel,
     MatButtonModule,
     FormsModule,
+    NgIf,
   ],
   templateUrl: './prediction-form.component.html',
   styleUrl: './prediction-form.component.css',
 })
 export class PredictionFormComponent {
+  private destroy$ = new Subject<void>();
+
+  matchId: number;
   homeTeamScore: number;
   awayTeamScore: number;
 
+  errorMessage: string | null = null;
+  isError = false;
+
   constructor(
+    private apiService: ApiService,
     public dialogRef: MatDialogRef<PredictionFormComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Match
   ) {
+    this.matchId = data.id;
     this.homeTeamScore = data.homeTeamScore;
     this.awayTeamScore = data.awayTeamScore;
   }
 
   onSubmit() {
-    const prediction = {
-      matchId: this.data.id,
-      homeTeamScore: this.homeTeamScore,
-      awayTeamScore: this.awayTeamScore,
-    };
-    // Call backend service to save prediction
-    console.log('Prediction Submitted: ', prediction);
-    this.dialogRef.close();
+    if (this.isValidForm()) {
+      this.apiService
+        .createPrediction(this.matchId, this.homeTeamScore, this.awayTeamScore)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (prediction) => {
+            console.log('Prediction Created', prediction);
+            this.dialogRef.close();
+          },
+          error: (error) => {
+            console.error('Failed to create prediction', error.error);
+            this.errorMessage = error.error;
+            this.isError = true;
+          },
+        });
+    }
+  }
+
+  isValidForm(): boolean {
+    return this.homeTeamScore >= 0 && this.awayTeamScore >= 0;
   }
 }
