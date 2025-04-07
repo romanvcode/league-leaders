a674498a6939cdf28751641712de9fc701c1620c
Roman Vorobel <rvorobel@leobit.com>
2025-04-07 15:36:48 +0300

    Refactor constant strings to separate classes

---
+++ api/src/LeagueLeaders.Application/Common/Constants/ExceptionMessages.cs
@@ -0,0 +1,24 @@
+﻿namespace LeagueLeaders.Application.Common.Constants;
+
+public static class ExceptionMessages
+{
+    public const string SportradarIdIncorrectFormat = "Sportradar ID is not in the correct format.";
+    public const string StartDateIncorrectFormat = "Start date is not in the correct format.";
+    public const string EndDateIncorrectFormat = "End date is not in the correct format.";
+    public const string SeasonIdNotFound = "Season ID is not found.";
+    public const string DateIncorrectFormat = "Date is not in the correct format.";
+    // "Stage ID is not found."
+    public const string StageIdNotFound = "Stage ID is not found.";
+    // "Home team ID is not found."
+    public const string HomeTeamIdNotFound = "Home team ID is not found.";
+    // "Away team ID is not found."
+    public const string AwayTeamIdNotFound = "Away team ID is not found.";
+    // "Venue ID is not found."
+    public const string VenueIdNotFound = "Venue ID is not found.";
+    // "Referee ID is not found."
+    public const string RefereeIdNotFound = "Referee ID is not found.";
+    // "Match ID is not found."
+    public const string MatchIdNotFound = "Match ID is not found.";
+    // "Team ID is not found."
+    public const string TeamIdNotFound = "Team ID is not found.";
+}
---
+++ api/src/LeagueLeaders.Application/Common/Constants/SyncLogStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace LeagueLeaders.Application.Common.Constants;
+
+public static class SyncLogStatus
+{
+    public const string Success = "Success";
+    public const string Failure = "Failure";
+}
