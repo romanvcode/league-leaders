97e83595432542063d8fc548e72e585f3529818a
Roman Vorobel <rvorobel@leobit.com>
2025-04-11 18:24:09 +0300

    Update mat-card visual styles

--- ui/src/app/features/predictions/predictions.component.ts
+++ ui/src/app/features/predictions/predictions.component.ts
@@ -3,6 +3,7 @@ import { NextMatchComponent } from '@shared/next-match/next-match.component';
 import { SearchInputComponent } from '@shared/search-input/search-input.component';
 import { TeamInfoComponent } from '@shared/team-info/team-info.component';
 import { PredictionsResultsComponent } from '@shared/predictions-results/predictions-results.component';
+import { StandingTableComponent } from '@shared/standing-table/standing-table.component';
 
 @Component({
   selector: 'app-predictions',
@@ -12,6 +13,7 @@ import { PredictionsResultsComponent } from '@shared/predictions-results/predict
     SearchInputComponent,
     TeamInfoComponent,
     PredictionsResultsComponent,
+    StandingTableComponent,
   ],
   templateUrl: './predictions.component.html',
   styleUrl: './predictions.component.css',
